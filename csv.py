ca2188604e5bccbd06d40ac15db50dfe242fde1c
ivan ng <40282336+ivanKng@users.noreply.github.com>
2021-03-06 21:21:27 -0500

    netflix movie list

---
+++ csv.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+
+#with open('./netflix_titles.csv', newline='') as f:
+#	reader = csv.reader(f)
+#	for row in reader:
+#		print(row[2])
+
+pd.read_csv('https://raw.githubusercontent.com/CalebCurry/python/master/netflix_titles.csv')
+
